cf0552e7065aa616e047ef75f219086bbbcee33d
stino-x <107426247+stino-x@users.noreply.github.com>
2024-02-29 18:39:07 +0100

    finished menu animation

--- src/components/listitems.tsx
+++ src/components/listitems.tsx
@@ -19,8 +19,18 @@ export default function Listitems() {
     { id: 8, text: 'Item 8' },
     { id: 9, text: 'Item 9' },
     { id: 10, text: 'Item 10' },
-    // Add more items as needed
+    // { id: 11, text: 'Item 11' },
+    // { id: 12, text: 'Item 12' },
+    // { id: 13, text: 'Item 13' },
+    // { id: 14, text: 'Item 14' },
+    // { id: 15, text: 'Item 15' },
+    // { id: 16, text: 'Item 16' },
+    // { id: 17, text: 'Item 17' },
+    // { id: 18, text: 'Item 18' },
+    // { id: 19, text: 'Item 19' },
+    // { id: 20, text: 'Item 20' },
   ];
+  
 
   const [showAllItems, setShowAllItems] = useState(false);
 
@@ -31,21 +41,25 @@ export default function Listitems() {
   return (
     <>
       {/* flex items-center w-2/3 flex-wrap gap-3 BREAKPOINT FOR LIST */}
-      <div className="flex justify-between mb-4">
-        <ul className="flex items-center gap-3">
-          {items.map((item) => (
-            <li key={item.id} className="roboto menu-link font-thin hover:font-bold">
-              {item.text}
-            </li>
-          ))}
-            <li className="flex" onClick={toggleShowAllItems}>
-            <span className="roboto">{showAllItems ? 'LESS' : 'MORE'}</span>  
-            <IoIosArrowForward
-              className={`text-2xl flex-2 cursor-pointer transform ${showAllItems ? 'rotate-90-left-cw' : 'rotate-90-right-cw'}`}
-             />
-            </li>
-        </ul>
-        <span className="hover:font-bold contact row-span-1 pointer max-[768px]:hidden">CONTACT</span>
+      {/* className={` ${showAllItems && items.length > 6 ? 'grid grid-cols-2 grid-rows-2' : 'flex justify-between mb-4'}`} */}
+      {/* flex justify-between mb-4 */}
+      {/* grid grid-cols-2 grid-rows-2 */}
+      <div className='flex justify-between mb-4'>
+      <ul className={` ${showAllItems && items.length > 5 ? 'grid grid-rows-2' : 'flex items-center gap-3'} ${showAllItems && items.length > 10 ? 'grid grid-rows-3' : 'flex items-center gap-3'}`}>
+        {items.map((item: ListItem, index: number) => (
+          <li key={item.id} className={`roboto menu-link font-thin hover:font-bold ${index > 5 && !showAllItems ? 'hidden' : ''} ${index >= 5 && showAllItems ? 'slide-in-top' : ''} ${index < 5 ? 'row-start-1 row-end-1' : ''} ${index >= 5 && index <= 10 ? 'row-start-2 row-end-2' : index > 12 ? 'row-start-3 row-end-3 w-24' : ''}`}>
+            {item.text}
+          </li>
+        ))}
+        <li className='flex' onClick={toggleShowAllItems}>
+          <span className="roboto">{showAllItems ? 'LESS' : 'MORE'}</span>  
+          <IoIosArrowForward
+            className={`flex-2 w-6 h-6 cursor-pointer transform ${showAllItems ? 'rotate-90-left-cw' : 'rotate-90-right-cw'}`}
+          />
+        </li>
+      </ul>
+
+        <span className="hover:font-bold contact row-span-1 pointer justify-self-end max-[768px]:hidden">CONTACT</span>
       </div>
     </>
   );
